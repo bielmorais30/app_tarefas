3a068ca8e6be7c39e2cbfc08fdade96641ca3afa
Gabriel Morais De Oliveira <bielmorais434@gmail.com>
2024-10-21 00:35:06 -0300

    Commit inicial -  Formulário

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Acesse sua conta</h1>
+    <form action="" method="post">
+    <p>
+        <label for="email">E-mail</label>
+        <input type="text" name="email">
+    </p>
+    <p>
+        <label for="senha">Senha</label>
+        <input type="password" name="semha">
+    </p>
+    <p>
+        <button type="ssubmit">Emtrar</button>
+    </p>
+    </form>
+</body>
+</html>
