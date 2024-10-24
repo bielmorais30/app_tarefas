3e1264ed7ad9b302cd23881bb3e1ebc9ce55ffee
Gabriel Morais De Oliveira <bielmorais434@gmail.com>
2024-10-24 03:14:32 -0300

    style: pagina home estilizada com bootstrap

--- src/views/home.php
+++ src/views/home.php
@@ -3,15 +3,14 @@
 
 require_once __DIR__ . '/../config/conexao.php';
 
-
-if(isset($_POST['email']) || isset( $_POST['password'])) {
+if (isset($_POST['email']) || isset($_POST['password'])) {
     $email = $_POST['email'] ?? "";
     $senha = $_POST['senha'] ?? "";
-    if(strlen($email) == 0) {
-        echo'Preencha seu email!';
-    }elseif(strlen($senha) == 0) {
+    if (strlen($email) == 0) {
+        echo 'Preencha seu email!';
+    } elseif (strlen($senha) == 0) {
         echo 'Preencha sua senha!';;
-    }else{
+    } else {
         $email = $mysqli->real_escape_string($_POST['email']); // retira os caracterers especiais
         $senha = $mysqli->real_escape_string($_POST['senha']); // não é a forma mais segura de previnir SQLINJECTION
 
@@ -21,47 +20,59 @@ if(isset($_POST['email']) || isset( $_POST['password'])) {
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if($result->num_rows > 0){
+        if ($result->num_rows > 0) {
             $usuario = $result->fetch_assoc();
-            
-           if(!isset($_SESSION)){
-            session_start();
-           }
 
-           $_SESSION['id'] = $usuario['id'];
-           $_SESSION['nome'] = $usuario['nome'];
-           
-           header("Location: index.php?page=painel");
+            if (!isset($_SESSION)) {
+                session_start();
+            }
 
-        }else{
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: index.php?page=painel");
+        } else {
             echo "Falha ao logar, email ou senha incorretos!";
         }
-
     };
 }
 
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <style>
+        body{
+            background-image: linear-gradient(45deg, orange, blue);
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Acesse sua conta</h1>
-    <form action="" method="post">
-    <p>
-        <label for="email">E-mail</label>
-        <input type="text" name="email">
-    </p>
-    <p>
-        <label for="senha">Senha</label>
-        <input type="password" name="senha">
-    </p>
-    <p>
-        <button type="submit">Entrar</button>
-    </p>
-    </form>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="bg-light p-4 rounded shadow">
+            <h1 class="text-center">Acesse sua conta</h1>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="text" name="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" class="form-control" required >
+                </div>
+                <div class="form-group mt-3">
+                    <button type="submit" class="btn btn-primary">Entrar</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
 </body>
+
 </html>
