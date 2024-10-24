ab80f0a5582d3adc8f87575688dc9c98691e0720
Gabriel Morais De Oliveira <bielmorais434@gmail.com>
2024-10-24 00:06:25 -0300

    refactor: Organizando as dependências do projeto corretamente

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-include("conexao.php");
+require_once __DIR__ . '/../src/config/conexao.php';
 
 if(isset($_POST['email']) || isset( $_POST['password'])) {
     $email = $_POST['email'] ?? "";
